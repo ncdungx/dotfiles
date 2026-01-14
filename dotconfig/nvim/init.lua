2901d74cd0204f9780578d250d6ff7fa70a11a7f
dungx2004 <88658472+dungx2004@users.noreply.github.com>
2026-01-15 01:06:59 +0700

    Update Neovim to use for LaTeX only

--- dotconfig/nvim/init.lua
+++ dotconfig/nvim/init.lua
@@ -2,68 +2,48 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug('rebelot/kanagawa.nvim')
 Plug('windwp/nvim-autopairs')
-Plug('h-hg/fcitx.nvim')
 Plug('nvim-treesitter/nvim-treesitter')
-Plug('neovim/nvim-lspconfig')
-Plug('williamboman/mason.nvim')
-Plug('williamboman/mason-lspconfig.nvim')
+Plug('lervag/vimtex')
 Plug('hrsh7th/nvim-cmp')
-Plug('hrsh7th/cmp-nvim-lsp')
-Plug('RaafatTurki/hex.nvim')
+Plug('micangl/cmp-vimtex')
 vim.call('plug#end')
 
 vim.o.rnu = true
 vim.o.nu = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
+vim.o.linebreak = true
+vim.o.breakindent = true
 
 vim.g.mapleader = ' '
-vim.cmd('nnoremap <silent> <leader>s :w<CR>')
-vim.cmd('nnoremap <silent> <leader>h :noh<CR>')
-vim.cmd('nnoremap <silent> <leader>e :w<CR>:e<CR>')
-vim.cmd('nnoremap <silent> <leader>b :HexToggle<CR>')
+vim.g.maplocalleader = ' '
+vim.g.vimtex_view_method = 'sioyek'
+
+vim.keymap.set('n', 'j', 'gj')
+vim.keymap.set('n', 'k', 'gk')
 
 vim.cmd.colorscheme('kanagawa')
 
 require('nvim-autopairs').setup()
 
-require('mason').setup()
-require('mason-lspconfig').setup()
-vim.diagnostic.config({ signs = false, severity_sort = true, jump = {float = true} })
-
 local cmp = require('cmp')
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			vim.snippet.expand(args.body)
-		end,
-	},
-	mapping = cmp.mapping.preset.insert({
+	mapping = ({
 		['<M-d>'] = cmp.mapping.scroll_docs(5),
 		['<M-u>'] = cmp.mapping.scroll_docs(-5),
 		['<M-e>']= cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({select = true}),
 		['<M-j>'] = cmp.mapping.select_next_item(),
 		['<M-k>'] = cmp.mapping.select_prev_item(),
+		['<M-Space>'] = cmp.mapping.complete()
 	}),
-	sources = cmp.config.sources({
-		{name = 'nvim_lsp'},
+	sources = ({
 		{name = 'buffer'},
 	}),
-	formatting = {
-		format = function(_, item)
-			local MAX_LABEL_WIDTH = 20
-			local content = item.abbr
-			if #content > MAX_LABEL_WIDTH then
-				item.abbr = vim.fn.strcharpart(content, 0,
-				MAX_LABEL_WIDTH) .. '…'
-			else
-				item.abbr = content .. (' '):rep(MAX_LABEL_WIDTH
-				- #content)
-			end
-			return item
-		end
-	}
 })
-
-require('hex').setup()
+cmp.setup.filetype('tex', {
+	sources = {
+		{name = 'vimtex'},
+		{name = 'buffer'},
+	},
+})
