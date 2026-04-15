523be0fce89e7b0c9105fce4b54a5277c9263007
Ngô Công Dũng <congdung100104@gmail.com>
2026-04-15 15:48:16 +0700

    Cậpnhật sau khi chuyển về win

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug('nvim-treesitter/nvim-treesitter', { ['branch'] = 'main', ['do'] = ':TSUpdate' })
 Plug('windwp/nvim-autopairs')
-Plug('keaising/im-select.nvim')
 Plug('rebelot/kanagawa.nvim')
 Plug('lervag/vimtex')
 Plug('hrsh7th/nvim-cmp')
@@ -18,17 +17,15 @@ vim.o.smartcase = true
 vim.o.linebreak = true
 vim.o.breakindent = true
 
-vim.cmd('set keymap=vietnamese-telex_utf-8')
-vim.cmd('autocmd InsertLeave * set keymap=vietnamese-telex_utf-8')
-vim.cmd('autocmd InsertEnter * set keymap=')
-require('im_select').setup()
-
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 vim.g.vimtex_view_method = 'sioyek'
 
 vim.cmd('nnoremap j gj')
 vim.cmd('nnoremap k gk')
 vim.cmd('nnoremap <leader>h :nohl<CR>')
 
+require('kanagawa').setup({ statementStyle = {bold = true} })
 vim.cmd.colorscheme('kanagawa')
 
 local npairs = require('nvim-autopairs')
@@ -54,8 +51,7 @@ cmp.setup({
 		['<M-j>'] = cmp.mapping.select_next_item(),
 		['<M-k>'] = cmp.mapping.select_prev_item(),
 		['<CR>'] = cmp.mapping.confirm({select = true}),
-		--['<M-CR>'] = cmp.mapping.complete(),
-		['<Tab>']= cmp.mapping.abort(),
+		['<Esc>']= cmp.mapping.abort(),
 		['<M-d>'] = cmp.mapping.scroll_docs(5),
 		['<M-u>'] = cmp.mapping.scroll_docs(-5),
 	}),
@@ -85,8 +81,6 @@ cmp.setup.filetype('tex', {
 require('lualine').setup({
 	options = {
 		icons_enabled = false,
-		component_separators = {left = '', right = ''},
-		section_separators = {left = '', right = ''},
 	},
 	sections = {
 		lualine_x = {},
@@ -94,3 +88,30 @@ require('lualine').setup({
 		lualine_z = {'location', 'progress'},
 	}
 })
+
+-- Workaround, used in paired with EVKey:
+-- 1033 is English (US)
+-- 8192 is English (World)
+local vie = '1033' 
+local eng = '8192'
+local current_kb = ''
+local function switch_im(kb)
+	return function()
+		vim.fn.jobstart({'im-select.exe', kb}, {
+			on_exit = function()
+				current_kb = kb
+				vim.fn.system('sleep 0.1')
+				vim.o.guicursor = vim.o.guicursor
+			end
+		})
+	end
+end
+
+vim.api.nvim_create_autocmd('VimEnter', { callback = switch_im(eng) })
+vim.api.nvim_create_autocmd('VimLeave', { callback = switch_im(vie) })
+vim.api.nvim_create_autocmd('InsertEnter', { callback = switch_im(vie) })
+vim.api.nvim_create_autocmd('InsertLeave', { callback = switch_im(eng) })
+vim.api.nvim_create_autocmd('CmdlineEnter', { callback = switch_im(vie) })
+vim.api.nvim_create_autocmd('CmdlineLeave', { callback = switch_im(eng) })
+vim.api.nvim_create_autocmd('FocusLost', { callback = switch_im(vie) })
+vim.api.nvim_create_autocmd('FocusGained', { callback = switch_im(current_kb) })
